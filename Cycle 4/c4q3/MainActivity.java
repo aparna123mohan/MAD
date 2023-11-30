68bd5a27f218a623d336bcee42f57d8d1eec2177
aparna123mohan <125428702+aparna123mohan@users.noreply.github.com>
2023-11-30 09:50:17 +0530

    Add files via upload

---
+++ Cycle 4/c4q3/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.style1);
+        setContentView(R.layout.activity_main);
+    }
+}
+
